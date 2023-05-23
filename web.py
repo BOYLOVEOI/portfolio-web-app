b8d995f07ec79ba5711883e13f8a2b54dbb05a09
Paul Lavoie <paullavoie2332@gmail.com>
2023-05-23 17:50:10 -0500

    Rendered Link to Source Code

--- web.py
+++ web.py
@@ -21,23 +21,36 @@ with c2:
     content = """Hello, my name is Paul Lavoie, a current student studying Information Technology at George Mason University. I created this portfolio web-app, specifically through the Streamlit module, to keep records and showcase projects that I have developed throughout my academic career. I have interned at TGI Fridays for their Digital Team and am excited to seek out and find new opportunities that may come my way! """
     c2.info(content)
 
+# Sectioning off my applications list
 st.title("My Application")
 
-c1,c2 = st.columns(2)
+# Creating new columns (to list the apps)
+c3,c4 = st.columns(2)
 
+# Going through each row in the dataframe
 for index, row in df.iterrows():
-    with c1:
+    # With C3 (adding onto column 3)
+    with c3:
+        # If index+1 of row is not evenly divisible by 2 (odd numbers)
         if (index + 1) % 2 != 0:
-            c1.header(df['title'][index])
-            c1.image('images/{}.png'.format(index+1))
-            c1.write(df['description'][index])
+            # Create header with the title of the row at the current index
+            c3.header(df['title'][index])
+            # Render image of the row at the current index
+            c3.image('images/{}.png'.format(index+1))
+            # Render description of row at the current index
+            c3.write(df['description'][index])
+            # Render link to source code 
+            c3.write("[Source Code]({})".format(df['url'][index]))
             
+    # Same process as above for column 4
+    with c4:
+        if (index + 1) % 2 == 0:  
+            c4.header(df['title'][index])
+            c4.image('images/{}.png'.format(index+1))
+            c4.write(df['description'][index])
+            c4.write("[Source Code]({})".format(df['url'][index]))
 
-    with c2:
-        if (index + 1) % 2 == 0:
-            c2.header(df['title'][index])
-            c2.image('images/{}.png'.format(index+1))
-            c2.write(df['description'][index])
+
 
 
 
